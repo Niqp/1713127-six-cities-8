c47da6883bd14a723d14aa5420740574abcfdfd3
Nikita Yupatov <hukull3@gmail.com>
2021-11-16 20:34:23 +0300

    Integrated react-router-dom

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainPage from '../main-page/main-page';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import Error404 from '../error-404/error-404';
+import LoginPage from '../login/login';
+import RoomPage from '../room/room';
+import FavoritesPage from '../favorites/favorites';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   cardsToRender: number;
@@ -6,7 +13,30 @@ type AppScreenProps = {
 
 function App({cardsToRender}: AppScreenProps): JSX.Element {
   return (
-    <MainPage cardsToRender={cardsToRender} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Root}>
+          <MainPage
+            cardsToRender={cardsToRender}
+          />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <LoginPage />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <RoomPage />
+        </Route>
+        <PrivateRoute
+          exact path={AppRoute.Favorites}
+          render={() => <FavoritesPage />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route>
+          <Error404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/error-404/error-404.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function Error404(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Link className="header__logo-link" to={AppRoute.Root}>
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">404</h1>
+            <h2>Page not found.</h2>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <Link className="locations__item-link" to={AppRoute.Root}>
+                <span>Back to main page</span>
+              </Link>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Error404;
